0277adc9a17cc0ac3e2eb99f38c50fad14699445
JoyrideGames <tjcgjohnson@gmail.com>
2017-08-22 12:09:45 +1000

    new stuff 22/8

---
+++ Assets/-Breakout/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class Ball : MonoBehaviour
+    {
+        public float speed = 5f;
+
+        private Vector3 velocity; //Velocity of the ball (Direction X Speed
+
+
+        public void Fire(Vector3 direction)
+        {
+            velocity = direction * speed;
+        }
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            //Grab contact point of collision
+            ContactPoint2D contact = col.contacts[0];
+            //Calculate the reflection point of the ball using the velocity & contact normal
+            Vector3 reflect = Vector3.Reflect(velocity, contact.normal);
+            //Calculate new velocity for reflection multiply by the same speed (velocity.magnitude)
+            velocity = reflect.normalized * velocity.magnitude;
+        }
+
+        private void Update()
+        {
+            //Move ball using velocity & deltaTime
+            transform.position += velocity * Time.deltaTime;
+        }
+
+
+
+    }
+
+}
---
+++ Assets/-Breakout/Scripts/Paddle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout { 
+
+public class Paddle : MonoBehaviour {
+
+        public float movementSpeed = 20f;
+        public Ball currentBall; //Ball that should be attached to the paddle as a child
+                                 //public Vector2[] directions; // List of directions for the ball to choose from
+
+        //Directions array defaults to two values
+        public Vector2[] directions = new Vector2[]
+        {
+            new Vector2(-0.5f, 0.5f),
+            new Vector2(0.5f, 0.5f)
+        };
+
+        void Start()
+        {
+            //Grabs currentBall from the children of the Paddle
+            currentBall = GetComponentInChildren<Ball>();
+        }
+
+        void Fire()
+        {
+            //Detach as child
+            currentBall.transform.SetParent(null);
+            // Generate random dir from list of directions
+            Vector3 randomDir = directions[Random.Range(0, directions.Length)];
+            // Fire off ball in randomDirection
+            currentBall.Fire(randomDir);
+        }
+
+        void CheckInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            Vector3 force = transform.right * inputH;
+            //Apply movement speed to force
+            force *= movementSpeed;
+            //Apply delta time to force
+            force *= Time.deltaTime;
+            //Add force to position
+            transform.position += force;
+        }
+		
+        void Update()
+        {
+            CheckInput();
+            Movement();
+        }
+
+
+	}
+}
